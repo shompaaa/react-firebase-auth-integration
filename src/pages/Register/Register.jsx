091cc69b8af4646905bce2cd131ce70b068f808c
shompaaa <shompaakter334@gmail.com>
2026-04-09 12:30:23 +0600

    register page password validation done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -7,24 +7,49 @@ import { IoEyeOff } from "react-icons/io5";
 import { AuthContext } from "../../context/AuthContext/AuthContext";
 
 const Register = () => {
-  const [showPassword, setShowPassword] = useState(false);
   const { createUser } = useContext(AuthContext);
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
 
   const togglePassword = () => {
     setShowPassword(!showPassword);
   };
+
+  //Password Validation RegEx
+  const testCase = /^(?=.*[a-z])(?=.*[A-Z]).+$/;
+  const testSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/;
 
   const handleRegister = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(name, email, password);
+    const confirmPassword = e.target.confirmPassword.value;
+
+    //Password Validation
+    if (!name) {
+      setError("Name is required");
+      return;
+    } else if (password.length < 6) {
+      setError("Password must be at least 6 characters");
+      return;
+    } else if (!testCase.test(password)) {
+      setError("Password must have one upper/lower case");
+      return;
+    } else if (!testSpecialCharacter.test(password)) {
+      setError("Password should have at least one special character");
+      return;
+    } else if (password !== confirmPassword) {
+      setError("Password didn't match");
+      return;
+    }
 
     createUser(email, password)
       .then((result) => {
         console.log(result);
-        e.target.reset()
+        e.target.reset();
+        //Reset Error
+        setError("");
       })
       .catch((error) => console.log(error));
 
@@ -56,6 +81,7 @@ const Register = () => {
                   name="email"
                   className="input"
                   placeholder="Email"
+                  required
                 />
                 {/* Password Field */}
                 <label className="label">Password</label>
@@ -74,6 +100,24 @@ const Register = () => {
                     )}
                   </button>
                 </div>
+                {/* Confirm Password Field */}
+                <label className="label">Confirm Password</label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="confirmPassword"
+                    className="input"
+                    placeholder="Password"
+                  />
+                  {/* <button onClick={togglePassword}>
+                    {showPassword ? (
+                      <IoEye size={20} className="absolute top-3 right-5" />
+                    ) : (
+                      <IoEyeOff size={20} className="absolute top-3 right-5" />
+                    )}
+                  </button> */}
+                </div>
+                {error && <small className="text-red-500">{error}</small>}
                 <button className="btn btn-neutral mt-4">Register</button>
               </fieldset>
             </form>
